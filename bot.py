c8839526afcd28651253e7ef0d141f39edd74fb3
shirtydiamond33 <kyrixty@gmail.com>
2021-11-29 20:05:12 -0500

    you can't see my token!

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
+import os
+
 from discord.ext import commands
+from dotenv      import load_dotenv
 
 bot = commands.Bot(command_prefix="?")
+
+# Assume that the .env file is always going to be in the same directory as bot.py
+root = os.path.dirname(__file__) + "/" # root dir
+load_dotenv(f"{root}.env")
 
 @bot.event
 async def on_ready():
   print("Connected to Discord!")
 
-bot.run("TOKEN")
+bot.run(os.getenv("TOKEN"))
